0c807cdefd6c9e693c46dad251e2a9b848790b15
AbinMuds <abinashmudbhari@gmail.com>
2023-05-22 02:35:20 -0600

    carrying out Crud in service

---
+++ src/app/employee-service.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './model/employee.model';
+
+const baseUrl = 'http://localhost:8000/api/v1/employee';
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeServiceService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(baseUrl);
+  }
+
+  get(id: number): Observable<Employee> {
+    return this.http.get<Employee>(`${baseUrl}/${id}`);
+  }
+
+  storeData(data: any): Observable<any> {
+    console.log(data);
+    return this.http.post(baseUrl, data);
+  }
+}
