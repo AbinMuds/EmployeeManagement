8ce59c1cf3e90a1211e22ba46ae8a09b259b85e5
AbinMuds <abinashmudbhari@gmail.com>
2023-05-22 02:30:43 -0600

    added code to carry out process to store data and also clear the data when adding another employee

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-
+import { Employee } from '../model/employee.model';
+import { EmployeeServiceService } from '../employee-service.service';
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  styleUrls: ['./add-employee.component.css'],
 })
 export class AddEmployeeComponent {
+  employee: Employee = {
+    firstName: '',
+    lastName: '',
+    dob: new Date(),
+    hireDate: new Date(),
+  };
+  submitted = false;
 
+  constructor(private employeeService: EmployeeServiceService) {}
+  saveEmployee(): void {
+    const data = {
+      firstName: this.employee.firstName,
+      lastName: this.employee.lastName,
+      dob: this.employee.dob,
+      hireDate: this.employee.hireDate,
+    };
+
+    this.employeeService.storeData(data).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.submitted = true;
+      },
+      error: (e) => console.log(e),
+    });
+  }
+
+  clearInputData() {
+    this.submitted = false;
+    this.employee = {
+      firstName: '',
+      lastName: '',
+      dob: new Date(),
+      hireDate: new Date(),
+    };
+  }
 }
